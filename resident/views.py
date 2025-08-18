307020e73275fdfd6fb6ff945bdf0524628d89c1
yusuf usman <yusuf.usman1003@gmail.com>
2025-08-18 16:23:16 +0100

    update

--- resident/views.py
+++ resident/views.py
@@ -1,12 +1,29 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import User
 
+from .models import House
+from .serializers import HouseSerializer
 
 # Create your views here.
 
 
-@api_view()
-def create_resident(request):
-    return Response("create resident")
+@api_view(['POST'])
+def add_house(request):
+    serializer = HouseSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    user = get_object_or_404(User, pk=serializer.validated_data['user'])
+    if user.is_resident:
+        House.objects.create(
+            house_number=serializer.validated_data['house_number'],
+            address=serializer.validated_data['address'],
+            user=user
+        )
+
+        return Response(data={"message": "house added successfully"}, status=status.HTTP_201_CREATED)
+
+    return Response(data={"message": "Not allowed"}, status=status.HTTP_403_FORBIDDEN)
