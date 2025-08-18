67ba2b85c9039d83cbf03bc20eda3a744d35232e
yusuf usman <yusuf.usman1003@gmail.com>
2025-08-18 12:00:22 +0100

    update

--- resident/admin.py
+++ resident/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .models import User
+
 
 # Register your models here.
+
+# admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ['first_name', 'last_name', 'email','username','phone']
