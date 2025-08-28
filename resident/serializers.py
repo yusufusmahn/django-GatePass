db536cd58948bd5f3c6a234e47b45fc52250d70e
yusuf usman <yusuf.usman1003@gmail.com>
2025-08-28 11:09:50 +0100

    update

--- resident/serializers.py
+++ resident/serializers.py
@@ -2,10 +2,19 @@ from rest_framework import serializers
 
 from resident.models import House
 
+from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+
 
 class HouseSerializer(serializers.ModelSerializer):
     user = serializers.IntegerField()
     class Meta:
         model = House
-        fields = ['house_number', 'address', 'user']
+        fields = ['house_number', 'address']
 
+
+
+
+class UserCreateSerializer(BaseUserCreateSerializer):
+    class Meta(BaseUserCreateSerializer.Meta):
+        fields = ['username', 'first_name', 'last_name', 'email', 'password']
+        ...
