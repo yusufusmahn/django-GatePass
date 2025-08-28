1b9e1d85308b832c9472e4ff0cc872c9ee4cfb74
yusuf usman <yusuf.usman1003@gmail.com>
2025-08-28 12:18:07 +0100

    update

--- GatePass/settings.py
+++ GatePass/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+from datetime import timedelta
+
 from dotenv import load_dotenv
 from pathlib import Path
 
@@ -148,4 +150,9 @@ DJOSER = {
     'SERIALIZERS':{
         'user_create': 'resident.serializers.UserCreateSerializer',
     }
+}
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
 }
