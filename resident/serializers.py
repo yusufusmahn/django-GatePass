379ec4ca03c875a0d3618114238563eeea553c23
yusuf usman <yusuf.usman1003@gmail.com>
2025-08-28 12:20:08 +0100

    update

--- resident/serializers.py
+++ resident/serializers.py
@@ -6,11 +6,9 @@ from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
 
 
 class HouseSerializer(serializers.ModelSerializer):
-    user = serializers.IntegerField()
     class Meta:
         model = House
         fields = ['house_number', 'address']
-
 
 
 
