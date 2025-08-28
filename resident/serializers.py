8d666cc66ec25d630aea79a4e5a791e8f02b5bca
yusuf usman <yusuf.usman1003@gmail.com>
2025-08-28 11:13:55 +0100

    update

--- resident/serializers.py
+++ resident/serializers.py
@@ -16,5 +16,5 @@ class HouseSerializer(serializers.ModelSerializer):
 
 class UserCreateSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
-        fields = ['username', 'first_name', 'last_name', 'email', 'password']
-        ...
+        fields = ['username', 'first_name', 'last_name', 'email', 'password', 'phone']
+
