cef2d48eadf085b6d029bfb663288374cfa84f31
yusuf usman <yusuf.usman1003@gmail.com>
2025-08-28 11:09:59 +0100

    update

--- resident/views.py
+++ resident/views.py
@@ -2,17 +2,18 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from .models import User
-
+from rest_framework.permissions import IsAuthenticated
 from .models import House
 from .serializers import HouseSerializer
 
+
 # Create your views here.
 
-
 @api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def add_house(request):
     serializer = HouseSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
