7885681819adbea445a0e9682814d865a8439e5a
yusuf usman <yusuf.usman1003@gmail.com>
2025-08-29 16:13:31 +0100

    update

--- resident/urls.py
+++ resident/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 
+
+
 urlpatterns = [
     path('add/house', views.add_house, name='add_house'),
     path('invite', views.create_invite, name='create_invite'),
